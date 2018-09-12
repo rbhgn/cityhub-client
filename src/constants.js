ff7d94e0b3ad66f29649a2d94077444ca8933df6
rbhgn <robhgn@gmail.com>
2018-09-12 17:22:05 +0200

    Heroku

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const localStorageJwtKey = process.env.JWT_SECRET || 'currentUserJwt'
 
-  // export const baseUrl = 'http://localhost:4000'
-export const baseUrl = 'https://cityhubbackend.herokuapp.com'
+export const baseUrl = 'http://localhost:4000'
+// export const baseUrl = 'https://cityhubbackend.herokuapp.com'
 
 export const defaultImages = [
   'https://cityhub.com/content/uploads/2018/07/home-official-website-17-1384x984-c-default.jpg',
