578f76eb2f7bf8cf350d0734994c9c822048e39c
rbhgn <robhgn@gmail.com>
2018-09-17 06:50:11 +0200

    Added auth and location to scraper

--- src/actions/scraper.js
+++ src/actions/scraper.js
@@ -1,5 +1,7 @@
 import * as request from 'superagent'
 import { baseUrl } from '../constants.js'
+import { isExpired } from '../jwt'
+import { logout } from './users'
 
 export const NEW_SCRAPE_SESSION_SUCCESS = 'NEW_SCRAPE_SESSION_SUCCESS'
 export const GET_SCRAPE_SESSIONS_SUCCESS = 'GET_SCRAPE_SESSIONS_SUCCESS'
@@ -15,16 +17,28 @@ const getScrapeSessionsSuccess = (data) => ({
   payload: data
 })
 
-export const newScrapeSession = () => (dispatch) => {
+export const newScrapeSession = () => (dispatch, getState) => {
+  const state = getState()
+  if (!state.currentUser) return null
+  const jwt = state.currentUser.jwt
+  if (isExpired(jwt)) return dispatch(logout())
+  const location = state.currentUser.location
   request
   .post(`${baseUrl}/scrapesession`)
+  .set('Authorization', `Bearer ${jwt}`)
+  .send({location})
   .then(result => dispatch(newScrapeSessionSuccess(result.body)))
   .catch(err => console.log(err.status)) 
 }
 
-export const getScrapeSessions = () => (dispatch) => {
+export const getScrapeSessions = () => (dispatch, getState) => {
+  const state = getState()
+  if (!state.currentUser) return null
+  const jwt = state.currentUser.jwt
+  if (isExpired(jwt)) return dispatch(logout())
   request
   .get(`${baseUrl}/scrapesession`)
+  .set('Authorization', `Bearer ${jwt}`)
   .then(result => dispatch(getScrapeSessionsSuccess(result.body)))
   .catch(err => console.log(err.status)) 
 }
